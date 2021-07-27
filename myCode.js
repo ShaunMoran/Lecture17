c66ece3c299928b299cd2caa6bf34714180cd656
ShaunMoran <57466726+ShaunMoran@users.noreply.github.com>
2021-07-26 19:51:39 -0500

    update with json code

--- myCode.js
+++ myCode.js
@@ -1,2 +1,17 @@
 window.addEventListener('load', function() {
+    
+    fetch("https://handlers.education.launchcode.org/static/weather.json")
+    .then(function(response) { 
+        
+        response.json().then(function(jsonResponse) {
+          
+            let weatherOutputElement = document.getElementById("weather-output");
+
+            weatherOutputElement.innerHTML =
+            `<h3>${jsonResponse.status}</h3>
+            <p>Temp: ${jsonResponse.temp} F</p>
+            <p>Low/High: ${jsonResponse.tempMin} F / ${jsonResponse.tempMax} F</p>
+            <p>Wind Speed: ${jsonResponse.windSpeed} mph</p>`;
+        });
+    });
 });
